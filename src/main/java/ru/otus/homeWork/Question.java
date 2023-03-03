976aa31b6e1388abd8f30f185a649373bc82b35b
dmitrocenkoaleksandr <dmitrocenkoaleksandr7@gmail.com>
2023-03-03 15:05:19 +0300

    домашние задание №4

---
+++ src/main/java/ru/otus/homeWork/Question.java
@@ -0,0 +1,22 @@
+package ru.otus.homeWork;
+
+public class Question {
+
+        private final String[] testQuestion;
+
+        public Question(String[] testQuestion) {
+            this.testQuestion = testQuestion;
+        }
+
+        public void printQuestion() {
+            for (int i = 0; i < testQuestion.length; i++) {
+                System.out.println(testQuestion[i]);
+
+            }
+
+        }
+
+
+    }
+
+
